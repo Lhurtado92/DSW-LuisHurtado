0cc3d8da902f023fbb269aaae488408571e75700
galindezak <42718365+afrodriguezgal@users.noreply.github.com>
2022-12-20 21:15:25 -0500

    Cambio de formula de js
    Se cambia la formula ya que el resultado no era correcto.

--- ProgramacionSitiosWeb/Fase2/jscript.js
+++ ProgramacionSitiosWeb/Fase2/jscript.js
@@ -4,15 +4,20 @@ formulario.addEventListener("submit", (e) => {
   let valor_comp = document.getElementById("valor_compra").value;
   let porcen = document.getElementById("porcentaje").value;
   let iva = document.getElementById("iva").value;
-  let cien = 100;
-  
+ 
+  valorcom = valor_comp / 100 * porcen;
+  document.getElementById("rganancia").value=valorcom;
+
+  valorcom2 = valor_comp / 100 * iva + parseInt(valor_comp) + parseInt(valorcom);  
+  document.getElementById("rventa").value=valorcom2;
+
   //function operacion(){
-    resul1 = cien - porcen;
+   /* resul1 = cien - porcen;
     resul2 = resul1 / cien;
     resul3 = valor_comp / resul2;
-    resulend = (resul3 * iva) + resul3;
-    document.getElementById("rventa").value=resul3;
-    document.getElementById("rganancia").value=resul2;
+    resulend = (resul3 * iva) + resul3;*/
+    
+    
   //}
   //alert(valor_comp);
 });
