dce04c46d618513affa2120b23cb1acf61992c00
galindezak <42718365+afrodriguezgal@users.noreply.github.com>
2022-12-24 23:37:06 +0000

    Se agrega codigo de js de equivalencia, culminado

--- ProgramacionSitiosWeb/Fase2/jsequivalencia.js
+++ ProgramacionSitiosWeb/Fase2/jsequivalencia.js
@@ -1,26 +1,130 @@
-let btn_valor = document.getElementById("btn_convertir");
-btn_valor.addEventListener('click', convertir);
-txtResultado = document.getElementById("resultado");
+let valor = document.getElementById("valor1");
+valor.addEventListener('keyup', convertir);
+txtResultado = document.getElementById("result");
 
-seleccion1 = document.getElementById("seleccion1");
+seleccion1 = document.getElementById("unidad1");
 seleccion1.addEventListener('change', convertir);
 
-seleccion2 = document.getElementById("seleccion2");
+seleccion2 = document.getElementById("unidad2");
 seleccion2.addEventListener('change', convertir);
 
 let resultado;
 
 function convertir(){
-    if(btn_valor.value==''){
+    if(valor.value==''){
         return;
     }
-    ingrese_valor = btn_valor.value;
-    ingrese_valor = parseFloat(ingrese_valor);
 
-    if(seleccion1.value=="byte" && seleccion2.value=="byte"){
-        resultado = ingrese_valor / 1;
-        txtResultado.innerHTML = ingrese_valor + "byte/s equivale a " + resultado + "byte/s";
+    numero = valor.value;
+    numero = parseFloat(numero);
+
+    //calcula byte
+
+    if(seleccion1.value=="byte" && seleccion2.value=="kilobyte"){
+    resultado = numero / 1000;
+    txtResultado.innerHTML = numero + " byte/s equivale a " + resultado + " kilobyte/s";
     }
+
+    if(seleccion1.value=="byte" && seleccion2.value=="megabyte"){
+    resultado = numero / 1000000;
+    txtResultado.innerHTML = numero + " byte/s equivale a " + resultado + " megabyte/s";
+    }
+
+    if(seleccion1.value=="byte" && seleccion2.value=="gigabyte"){
+    resultado = numero / 1000000000;
+    txtResultado.innerHTML = numero + " byte/s equivale a " + resultado + " gigabyte/s";
+    }
+
+    if(seleccion1.value=="byte" && seleccion2.value=="terabyte"){
+    resultado = numero / 1000000000000;
+    txtResultado.innerHTML = numero + " byte/s equivale a " + resultado + " terabyte/s";
+    }
+
+//calcula kilobyte
+
+if(seleccion1.value=="kilobyte" && seleccion2.value=="byte"){
+    resultado = numero * 1000;
+    txtResultado.innerHTML = numero + " kilobyte/s equivale a " + resultado + " byte/s";
+    }
+
+    if(seleccion1.value=="kilobyte" && seleccion2.value=="megabyte"){
+    resultado = numero / 1000;
+    txtResultado.innerHTML = numero + " kilobyte/s equivale a " + resultado + " megabyte/s";
+    }
+
+    if(seleccion1.value=="kilobyte" && seleccion2.value=="gigabyte"){
+    resultado = numero / 1000000;
+    txtResultado.innerHTML = numero + " kilobyte/s equivale a " + resultado + " gigabyte/s";
+    }
+
+    if(seleccion1.value=="kilobyte" && seleccion2.value=="terabyte"){
+    resultado = numero / 1000000000;
+    txtResultado.innerHTML = numero + " byte/s equivale a " + resultado + " terabyte/s";
+    }
+
+    //calcular a megabyte
+
+    if(seleccion1.value=="megabyte" && seleccion2.value=="byte"){
+        resultado = numero * 1000000;
+        txtResultado.innerHTML = numero + " megabyte/s equivale a " + resultado + " byte/s";
+        }
+    
+        if(seleccion1.value=="megabyte" && seleccion2.value=="kilobyte"){
+        resultado = numero * 1000;
+        txtResultado.innerHTML = numero + " megabyte/s equivale a " + resultado + " kilobyte/s";
+        }
+    
+        if(seleccion1.value=="megabyte" && seleccion2.value=="gigabyte"){
+        resultado = numero / 1000;
+        txtResultado.innerHTML = numero + " megabyte/s equivale a " + resultado + " gigabyte/s";
+        }
+    
+        if(seleccion1.value=="megabyte" && seleccion2.value=="terabyte"){
+        resultado = numero / 1000000;
+        txtResultado.innerHTML = numero + " megabyte/s equivale a " + resultado + " terabyte/s";
+        }
+
+    //calcular a gigabyte
+
+    if(seleccion1.value=="gigabyte" && seleccion2.value=="byte"){
+        resultado = numero * 1000000000;
+        txtResultado.innerHTML = numero + " gigabyte/s equivale a " + resultado + " byte/s";
+        }
+    
+        if(seleccion1.value=="gigabyte" && seleccion2.value=="megabyte"){
+        resultado = numero * 1000;
+        txtResultado.innerHTML = numero + " gigabyte/s equivale a " + resultado + " megabyte/s";
+        }
+    
+        if(seleccion1.value=="gigabyte" && seleccion2.value=="kilobyte"){
+        resultado = numero * 1000000;
+        txtResultado.innerHTML = numero + " gigabyte/s equivale a " + resultado + " kilobyte/s";
+        }
+    
+        if(seleccion1.value=="gigabyte" && seleccion2.value=="terabyte"){
+        resultado = numero / 1000;
+        txtResultado.innerHTML = numero + " gigabyte/s equivale a " + resultado + " terabyte/s";
+        }
+
+    //calcular a terabyte
+
+    if(seleccion1.value=="terabyte" && seleccion2.value=="byte"){
+        resultado = numero * 1000000000000;
+        txtResultado.innerHTML = numero + " terabyte/s equivale a " + resultado + " byte/s";
+        }
+    
+        if(seleccion1.value=="terabyte" && seleccion2.value=="megabyte"){
+        resultado = numero * 1000000;
+        txtResultado.innerHTML = numero + " terabyte/s equivale a " + resultado + " megabyte/s";
+        }
+    
+        if(seleccion1.value=="terabyte" && seleccion2.value=="gigabyte"){
+        resultado = numero * 1000;
+        txtResultado.innerHTML = numero + " terabyte/s equivale a " + resultado + " gigabyte/s";
+        }
+    
+        if(seleccion1.value=="terabyte" && seleccion2.value=="kilobyte"){
+        resultado = numero * 1000000000;
+        txtResultado.innerHTML = numero + " terabyte/s equivale a " + resultado + " kilobyte/s";
+        }
 }
-
-
